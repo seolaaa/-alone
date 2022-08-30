f5a8663b6a2c888f6d43387215437a19e2a3c00a
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-08-30 22:34:20 +0900

    메인텍스트 애니메이션 수정완
    텍스트부분수정했음

--- js/main.js
+++ js/main.js
@@ -10,24 +10,22 @@ window.addEventListener('load',()=>{
 
   
 
-initEvent();
 
-function initEvent(){
-    setInterval(TextAni);
-}
-setInterval(TextAni);
-    
-        
-    
-     
-       
-   
+    let animation = true;
+
+    TextAni();
+
 
     function TextAni(){
+        console.log(1);
+        gsap.set(textWrap[0], {opacity:1});
         gsap.to(titleSpan[0],{opacity:1,duration:0.5,ease:'power1.out',delay:0.3})
         gsap.to(titleSpan[1],{opacity:1,duration:0.5,ease:'power1.out',delay:0.7})
         gsap.to(text[0],{opacity:1,duration:0.7,ease:'power1.out',delay:1.2})
         gsap.set(textWrap[0],{opacity:0,delay:5,onComplete:()=>{
+            gsap.set(titleSpan[0],{opacity:0});
+            gsap.set(titleSpan[1],{opacity:0});
+            gsap.set(text[0],{opacity:0});
             TextAni2();
         }})
     }
@@ -36,10 +34,15 @@ setInterval(TextAni);
          
     
     function TextAni2(){
+        console.log(2);
+        gsap.set(textWrap[1], {opacity:1});
         gsap.to(titleSpan[2],{opacity:1,duration:0.5,ease:'power1.out',delay:0.3})
         gsap.to(titleSpan[3],{opacity:1,duration:0.5,ease:'power1.out',delay:0.7})
         gsap.to(text[1],{opacity:1,duration:0.7,ease:'power1.out',delay:1.2})
         gsap.set(textWrap[1],{opacity:0,delay:5,onComplete:()=>{
+            gsap.set(titleSpan[2],{opacity:0})
+            gsap.set(titleSpan[3],{opacity:0})
+            gsap.set(text[1],{opacity:0})
             TextAni();
         }})
     }
