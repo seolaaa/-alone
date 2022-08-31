878418d0ebb79cc235062b4ef4179a9a613cabdb
seolaaa <109854037+seolaaa@users.noreply.github.com>
2022-08-31 13:12:01 +0900

    텍스트 js 높이수정

--- js/content.js
+++ js/content.js
@@ -4,10 +4,6 @@ window.addEventListener('load',()=>{
     const bigText = document.querySelectorAll('.Big-text')
     const smallText = document.querySelectorAll('.explanation-text')
 
-    
-    let height = window.innerHeight;
-
-    // alert(height)
 
     initEvent();
 
@@ -17,16 +13,15 @@ window.addEventListener('load',()=>{
     }
 
     function scrollContent(){
-        let scrollHeight = window.pageYOffset;
 
-        console.log(1);
+        let scrollHeight = window.pageYOffset;
 
         if(scrollHeight>=150){
             gsap.to(blueText[0],{opacity:1,top:0,duration:0.3,ease:'power3.out',delay:1})
             gsap.to(bigText[0],{opacity:1,top:15,duration:0.3,ease:'power3.out',delay:1.3})
             gsap.to(smallText[0],{opacity:1,top:60,duration:0.3,ease:'power3.out',delay:1.5})
         }
-        if(scrollHeight>=300){
+        if(scrollHeight>=900){
             gsap.to(blueText[1],{opacity:1,top:0,duration:0.3,ease:'power3.out',delay:1})
             gsap.to(bigText[1],{opacity:1,top:15,duration:0.3,ease:'power3.out',delay:1.3})
         }
