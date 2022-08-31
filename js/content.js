d2bf947cb61147eeb4b336bca34603e24e5194ff
seolaaa <109854037+seolaaa@users.noreply.github.com>
2022-08-31 17:39:01 +0900

    보더 애니메이션 수정완

--- js/content.js
+++ js/content.js
@@ -5,7 +5,7 @@ window.addEventListener('load',()=>{
     const smallText = document.querySelectorAll('.explanation-text')
     const icon = document.querySelectorAll('.icon')
 
-    console.log(icon.length);
+
 
 
     initEvent();
@@ -24,13 +24,13 @@ window.addEventListener('load',()=>{
             gsap.to(bigText[0],{opacity:1,top:15,duration:0.3,ease:'power3.out',delay:1.3})
             gsap.to(smallText[0],{opacity:1,top:60,duration:0.3,ease:'power3.out',delay:1.5})
         }
-        if(scrollHeight>=700){
-            gsap.to(icon[0],{scale:1,duration:0.7,ease:'power4.out',delay:0.2})
-            gsap.to(icon[2],{scale:1,duration:0.7,ease:'power4.out',delay:0.3})
-            gsap.to(icon[4],{scale:1,duration:0.7,ease:'power4.out',delay:0.4})
-            gsap.to(icon[1],{scale:1,duration:0.7,ease:'power4.out',delay:0.5})
-            gsap.to(icon[3],{scale:1,duration:0.7,ease:'power4.out',delay:0.6})
-            gsap.to(icon[5],{scale:1,duration:0.7,ease:'power4.out',delay:0.7})
+        if(scrollHeight>=1000){
+            gsap.to(icon[0],{scale:1,duration:0.7,ease:'power1.out',delay:0.2})
+            gsap.to(icon[2],{scale:1,duration:0.7,ease:'power1.out',delay:0.3})
+            gsap.to(icon[4],{scale:1,duration:0.7,ease:'power1.out',delay:0.4})
+            gsap.to(icon[1],{scale:1,duration:0.7,ease:'power1.out',delay:0.5})
+            gsap.to(icon[3],{scale:1,duration:0.7,ease:'power1.out',delay:0.6})
+            gsap.to(icon[5],{scale:1,duration:0.7,ease:'power1.out',delay:0.7})
         }
         if(scrollHeight>=1300){
             gsap.to(blueText[1],{opacity:1,top:0,duration:0.3,ease:'power3.out',delay:1})
