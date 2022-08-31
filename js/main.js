718dae80a8439fe2ae2a501d3e1103b2e223c31b
seolaaa <109854037+seolaaa@users.noreply.github.com>
2022-08-31 12:16:13 +0900

    span 수정완료

--- js/main.js
+++ js/main.js
@@ -3,15 +3,14 @@ window.addEventListener('load',()=>{
     const textWrap = document.querySelectorAll('.visual-text-wrap')
     const titleSpan = document.querySelectorAll('.visual-title>span')
     const text = document.querySelectorAll('.visual-text')
-    const st = document.querySelectorAll('.st')
-    const normal = document.querySelectorAll('.normal')
+   
 
+  
 
     let timer=null;
 
-    console.log(st.length)
-    console.log(normal.length)
-
+    // console.log(titleIn.length)
+  
 
   
 
@@ -24,12 +23,12 @@ window.addEventListener('load',()=>{
     function TextAni(){
         console.log(1);
         gsap.set(textWrap[0], {opacity:1});
-        gsap.to(normal[0],{opacity:1,scaleY:1,duration:0.5,ease:'power1.out',delay:0.3})
-        gsap.to(st[0],{opacity:1,scaleY:1,duration:0.5,ease:'power1.out',delay:0.7})
+        gsap.to(titleSpan[0],{opacity:1,scaleY:1,duration:0.5,ease:'power1.out',delay:0.3})
+        gsap.to(titleSpan[1],{opacity:1,scaleY:1,duration:0.5,ease:'power1.out',delay:0.7})
         gsap.to(text[0],{opacity:1,scaleY:1,duration:0.7,ease:'power1.out',delay:1.2})
         gsap.set(textWrap[0],{opacity:0,delay:5,onComplete:()=>{
-            gsap.set(normal[0],{opacity:0,scaleY:1.5,});
-            gsap.set(st[0],{opacity:0,scaleY:1.5,});
+            gsap.set(titleSpan[0],{opacity:0,scaleY:1.5});
+            gsap.set(titleSpan[1],{opacity:0,scaleY:1.5});
             gsap.set(text[0],{opacity:0,scaleY:1.5});
             TextAni2();
         }})
@@ -41,13 +40,13 @@ window.addEventListener('load',()=>{
     function TextAni2(){
         console.log(2);
         gsap.set(textWrap[1], {opacity:1});
-        gsap.to(normal[1],{opacity:1,scaleY:1,duration:0.5,ease:'power1.out',delay:0.3})
-        gsap.to(st[1],{opacity:1,scaleY:1,duration:0.5,ease:'power1.out',delay:0.7})
+        gsap.to(titleSpan[2],{opacity:1,scaleY:1,duration:0.5,ease:'power1.out',delay:0.3})
+        gsap.to(titleSpan[3],{opacity:1,scaleY:1,duration:0.5,ease:'power1.out',delay:0.7})
         gsap.to(text[1],{opacity:1,scaleY:1,duration:0.7,ease:'power1.out',delay:1.2})
         gsap.set(textWrap[1],{opacity:0,delay:5,onComplete:()=>{
-            gsap.set(normal[1],{opacity:0,scaleY:1.2,})
-            gsap.set(st[1],{opacity:0,scaleY:1.2,})
-            gsap.set(text[1],{opacity:0,scaleY:1.2})
+            gsap.set(titleSpan[2],{opacity:0,scaleY:1.5})
+            gsap.set(titleSpan[3],{opacity:0,scaleY:1.5})
+            gsap.set(text[1],{opacity:0,scaleY:1.5})
             TextAni();
         }})
     }
