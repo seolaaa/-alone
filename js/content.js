78048f40d8ca66b3798812f877552f5d92f7322e
seolaaa <109854037+seolaaa@users.noreply.github.com>
2022-09-01 15:35:06 +0900

    our pro js 부분 수정
    라인이팩트

--- js/content.js
+++ js/content.js
@@ -3,14 +3,17 @@ window.addEventListener('load',()=>{
     const blueText = document.querySelectorAll('.blue-text')
     const bigText = document.querySelectorAll('.Big-text')
     const smallText = document.querySelectorAll('.explanation-text')
+
     const icon = document.querySelectorAll('.icon')
+
     const stacoreText = document.querySelector('#stacore-title')
+    const line = document.querySelector('.line')
 
 
 
 
     let n = 0;
-    const content = 'THIS IS A STACORE!   '
+    const content = 'THIS IS A STACORE!'
 
     
 
@@ -42,6 +45,7 @@ window.addEventListener('load',()=>{
         if(scrollHeight>=1300){
             gsap.to(blueText[1],{opacity:1,top:0,duration:0.3,ease:'power3.out',delay:1})
             gsap.to(bigText[1],{opacity:1,top:15,duration:0.3,ease:'power3.out',delay:1.3})
+            gsap.to(line,{width:1000,duration:0.1,ease:'power4.out',delay:2})
         }
     }
 
@@ -53,12 +57,12 @@ window.addEventListener('load',()=>{
         if(scrollHeight>=1000){
 
             console.log(1);
-            gsap.to(icon[0],{top:0,opacity:1,duration:0.7,ease:'power1.out',delay:0.2})
-            gsap.to(icon[3],{top:0,opacity:1,duration:0.7,ease:'power1.out',delay:0.3})
-            gsap.to(icon[5],{top:0,opacity:1,duration:0.7,ease:'power1.out',delay:0.4})
-            gsap.to(icon[4],{bottom:0,opacity:1,duration:0.7,ease:'power1.out',delay:0.6})
-            gsap.to(icon[2],{bottom:0,opacity:1,duration:0.7,ease:'power1.out',delay:0.7})
-            gsap.to(icon[1],{bottom:0,opacity:1,duration:0.7,ease:'power1.out',delay:0.8})
+            gsap.to(icon[0],{top:0,opacity:1,duration:0.7,ease:'back.out',delay:0.2})
+            gsap.to(icon[3],{top:0,opacity:1,duration:0.7,ease:'back.out',delay:0.3})
+            gsap.to(icon[5],{top:0,opacity:1,duration:0.7,ease:'back.out',delay:0.4})
+            gsap.to(icon[4],{bottom:0,opacity:1,duration:0.7,ease:'back.out',delay:0.6})
+            gsap.to(icon[2],{bottom:0,opacity:1,duration:0.7,ease:'back.out',delay:0.7})
+            gsap.to(icon[1],{bottom:0,opacity:1,duration:0.7,ease:'back.out',delay:0.8})
             
            
         }
